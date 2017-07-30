cc0d30c53ed8ef8e81db4fea9a9ae7ec9a917686
Hernan Schmidt <lagartoflojo@users.noreply.github.com>
2017-07-30 17:51:15 +0200

    Improve test cases

--- spec/githubRegexSpec.js
+++ spec/githubRegexSpec.js
@@ -3,7 +3,7 @@ const matchRepo = imports.githubRegex.matchRepo;
 describe('githubRegex', function () {
   describe('matchRepo', function () {
     it('returns true for a valid repo', function () {
-      expect(matchRepo("abc/def")).toBe(true);
+      expect(matchRepo("abc/def-ghi_jkl")).toBe(true);
     });
 
     it('returns false for no slash nor repo', function () {
@@ -12,6 +12,10 @@ describe('githubRegex', function () {
 
     it('returns false for slash but no repo', function () {
       expect(matchRepo("abc/")).toBeFalsy();
+    });
+
+    it('returns false for an empty string', function () {
+      expect(matchRepo("")).toBeFalsy();
     });
   });
 });
